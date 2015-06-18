4c73f173f38ac0b292744214c5207ffc27a5eb47
Eric Ihli <ericihli@gmail.com>
2015-06-18 12:21:27 -0700

    (doc) Add note to start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# This file will be seeded with default values that each individual
+# developer should change when they first start
+# working on the repo. This file should be in the .gitignore
+# so usernames and passwords aren't commited to the repo.
+
 export MYSQL_USER=root
 export MYSQL_HOST=localhost
 export MYSQL_PASS=''
