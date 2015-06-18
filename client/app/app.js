0e1bb137f69efe5b783319638e6ef7793637a6b5
Devon <devonharvey@gmail.com>
2015-06-18 11:45:06 -0700

    (feat)Add routes for syllabus, grades, and attendance

--- client/app/app.js
+++ client/app/app.js
@@ -7,5 +7,18 @@ angular.module('classroom', [
   'ui.router'
 ])
 .config(function ($stateProvider, $urlRouterProvider) {
-
+  $urlRouterProvider.otherwise('/syllabus');
+  $stateProvider
+    .state('syllabus', {
+      url: '/syllabus',
+      templateUrl: '/syllabus/syllabus.html'
+    })
+    .state('grades', {
+      url: '/grades',
+      templateUrl: '/grades/grades.html'
+    })
+    .state('attendance', {
+      url: '/attendance',
+      templateUrl: '/attendance/attendance.html'
+    })
 });
