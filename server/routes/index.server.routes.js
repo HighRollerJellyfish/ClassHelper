aaef77f65c460ee9f64e60850b13305b9631a890
Eric Ihli <ericihli@gmail.com>
2015-06-19 15:52:40 -0700

    (refactor) Add static route.

--- server/routes/index.server.routes.js
+++ server/routes/index.server.routes.js
@@ -1,4 +1,7 @@
+var express = require('express');
 module.exports = function(app) {
   var index = require('../controllers/index.server.controller');
+  // This isn't getting triggered because the static middleware is catching this route. It
+  // could be commented out and the app will work just the same right now.
   app.get('/', index.render);
 };
