a6b5f9fd7f5dc7fdb18c6f86f9b961d175ee9ad4
Eric Ihli <ericihli@gmail.com>
2015-06-22 11:02:26 -0700

    (bug) syllabus.html was invoking lessons. Refactored some things and fixed it.

--- client/app/syllabus/syllabus.js
+++ client/app/syllabus/syllabus.js
@@ -1,8 +1,7 @@
 angular.module('classroom.syllabus', ['classroom.services'])
-.controller('SyllabusController', function ($scope, GetSyllabus) {
+.controller('SyllabusController', ['$scope', 'GetSyllabus', function ($scope, GetSyllabus) {
+  console.log(GetSyllabus);
   GetSyllabus.lessons().then(function(data) {
     $scope.lessons = data;
   });
-
-
-});
+}]);
