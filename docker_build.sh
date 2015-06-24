c1a798bbdcd0740b2499ca70041774aa78de2f9c
Eric Ihli <ericihli@gmail.com>
2015-06-24 16:12:00 -0700

    Hopefully final change to docker

--- docker_build.sh
+++ docker_build.sh
@@ -6,3 +6,5 @@ sleep 5
 echo "Docker containers up and running. Server is live!"
 docker ps
 echo "If you don't see any docker containers above... run ./docker_start.sh"
+echo "I tell you what... I'll go ahead and run it for you just in case..."
+./docker_start.sh
