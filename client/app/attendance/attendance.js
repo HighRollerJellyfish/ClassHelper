d124d29127f271e1485b445dcc369b631bc5ea9c
Devon <devonharvey@gmail.com>
2015-06-24 15:25:49 -0700

    (feat)Add attendance controller

--- client/app/attendance/attendance.js
+++ client/app/attendance/attendance.js
@@ -1,4 +1,13 @@
 angular.module('classroom.attendance', [])
-.controller('AttendanceController', function ($scope) {
-
+.controller('AttendanceController', function ($rootScope, $scope, $state, GetAttendance) {
+  $scope.isCollapsed = false;
+  if ($rootScope.currentUser.role === 'teacher') {
+    GetAttendance.allAttendance().then(function(data) {
+      $scope.attendance = angular.fromJson(data.data);
+    });
+  } else {
+    GetAttendance.attendanceForUser($rootScope.currentUser.username).then(function(data) {
+      $scope.attendance = angular.fromJson(data.data);
+    });
+  }
 });
