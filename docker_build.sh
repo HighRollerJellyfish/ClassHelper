91e8b85b188e499fea8f9b8ea53b7402feffe44a
Eric Ihli <ericihli@gmail.com>
2015-06-24 16:00:02 -0700

    docker

--- docker_build.sh
+++ docker_build.sh
@@ -2,6 +2,7 @@
 ./mysql_start.sh
 sleep 5
 ./node_start.sh
+sleep 5
 echo "Docker containers up and running. Server is live!"
 docker ps
 echo "If you don't see any docker containers above... run ./docker_start.sh"
