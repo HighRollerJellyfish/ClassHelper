186e2c6400c2148c3802243f37f4cc5f69ee0aa9
Devon <devonharvey@gmail.com>
2015-06-24 15:04:18 -0700

    (feat)Add placeholder grades table to schema for testing

--- server/config/schema.sql
+++ server/config/schema.sql
@@ -60,6 +60,31 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
+
+-- -----------------------------------------------------
+-- Table `grades`
+-- -----------------------------------------------------
+-- This is temporary and needs to represent real grades
+DROP TABLE IF EXISTS `grades` ;
+
+CREATE TABLE IF NOT EXISTS `grades` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `title` VARCHAR(45) NOT NULL COMMENT '',
+  `student` VARCHAR(45) NOT NULL COMMENT '',
+  `description` VARCHAR(140) NOT NULL COMMENT '',
+  `content` TEXT NOT NULL COMMENT '',
+  `start_date` DATETIME NOT NULL COMMENT '',
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP COMMENT '',
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC)  COMMENT '')
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
 -- -----------------------------------------------------
 -- Data for table `users`
 -- -----------------------------------------------------
@@ -105,4 +130,25 @@ INSERT INTO `lessons` (`title`, `description`, `content`, `start_date`, `created
 INSERT INTO `lessons` (`title`, `description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('Data Structures', 'Creation of various data structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
 
 COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `grades`
+-- -----------------------------------------------------
+-- This is temporary and needs to be replaced with real grades
+START TRANSACTION;
+USE `classroom`;
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('GradeAlgorithms', 'p','Analysis of time and space complexity of algorithms', 'Lots and lots of big O notations', '2015-07-01', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('N gradeData Structures', 'n', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('N gradeData Structures', 'n', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('gradeData Structures', 'p', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('gradeData Structures', 'p', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('gradeData Structures', 'p', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('N gradeData Structures', 'n', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('gradeData Structures', 'p', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('N gradeData Structures', 'n', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+INSERT INTO `grades` (`title`, `student`,`description`, `content`, `start_date`, `created_at`, `updated_at`) VALUES ('gradeData Structures', 'p', 'Creation of various gradedata structures.', 'Binary trees, hash tables, stacks, and queues oh my', '2015-07-08', NULL, NULL);
+
+COMMIT;
+
 
