e6f119eaecfe98591c1abca9e2b2684de835fd36
Eric Ihli <ericihli@gmail.com>
2015-06-26 13:02:04 -0700

    (feat) Docker
    Some bug where we are getting a 409 User already exists. error when I
    send these files to the server.

--- mysql_start.sh
+++ mysql_start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 docker stop classroom-db
 docker rm classroom-db
-docker build --no-cache -t eihli/mysql:v2 -f mysql_Dockerfile .
+# docker build -t eihli/mysql:v2 -f mysql_Dockerfile .
 docker run -d -p 3306:3306 --name classroom-db eihli/mysql:v2
-sleep 20
+sleep 10
 docker exec classroom-db /bin/bash -c "mysql -uroot -ppassword < schema.sql"
-sleep 5
