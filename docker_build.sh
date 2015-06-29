1e71766fecd0d6fd483d93b32d34bb52b02c26fc
Eric Ihli <ericihli@gmail.com>
2015-06-29 13:53:13 -0700

    (refactor) Fix small issues and comment docker files and scripts.

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+echo "Running ./mysql_build.sh"
 ./mysql_build.sh
+
+echo "Running ./node_build.sh"
 ./node_build.sh
-echo "Docker containers should be up and running."
+
+echo "Docker containers should be up and running. List of docker containers currently running:"
 docker ps
+
 echo "If you don't see any docker containers above... try running ./docker_start.sh"
--- mysql_start.sh
+++ mysql_start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Import environment variables with 'source config.sh'
 source config.sh
+
+echo "Stopping and removing any old classroom-db containers."
 docker stop classroom-db
 docker rm classroom-db
+
+echo "Running a new classroom/mysql:v1 container and naming it classroom-db."
 docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$CLASSROOM_MYSQL_PASS --name classroom-db classroom/mysql:v1
