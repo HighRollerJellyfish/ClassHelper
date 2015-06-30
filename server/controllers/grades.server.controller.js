47070f5cab3bb87a634d48a26214143925c93379
bdstein33 <bdstein33@gmail.com>
2015-06-30 14:53:46 -0700

    (doc) Add documentation to functions in the server

--- server/controllers/grades.server.controller.js
+++ server/controllers/grades.server.controller.js
@@ -1,17 +1,20 @@
 var Grade = require('../models/grade.server.model');
 
+// Returns an object of all grades
 exports.listAll = function(req, res, next) {
   new Grade().fetchAll().then(function(collection) {
     res.json(collection);
   });
 };
 
+// Returns an object of all grades for a given student
 exports.listForUser = function(student, req, res, next) {
   new Grade().where({student: student}).fetchAll().then(function(collection) {
     res.json(collection);
   });
 };
 
+// Greates a new grade
 exports.create = function(req, res, next) {
   Grade.add(req.body, function(model) {
     console.log(model);
