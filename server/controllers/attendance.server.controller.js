c8e598c90e156044c9f0017c6e5d5cdc88d11e59
bdstein33 <bdstein33@gmail.com>
2015-06-30 14:53:46 -0700

    (doc) Add documentation to functions in the server

--- server/controllers/attendance.server.controller.js
+++ server/controllers/attendance.server.controller.js
@@ -1,17 +1,20 @@
 var Attendance = require('../models/attendance.server.model');
 
+// Returns an object with all attendance data
 exports.listAll = function(req, res, next) {
   new Attendance().fetchAll().then(function(collection) {
     res.json(collection);
   });
 };
 
+// Returns an object with all attendance data for a specific user
 exports.listForUser = function(student, req, res, next) {
   new Attendance().where({student: student}).fetchAll().then(function(collection) {
     res.json(collection);
   });
 };
 
+// Creates new attendance data
 exports.create = function(req, res, next) {
   Attendance.add(req.body, function(model) {
     console.log(model);
