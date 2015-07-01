61b4251c39d2598b66ac3c8a69066ab4f46ff02e
Dianna Faulk <diannafaulk@gmail.com>
2015-06-30 19:21:22 -0700

    Show all student grades as a line chart

--- client/app/grades/grades.js
+++ client/app/grades/grades.js
@@ -31,6 +31,7 @@ angular.module('classroom.grades', [])
     Grades.getAll().then(function(data) {
       var svg = dimple.newSvg(".grades", 1000, 800);
       var gradesData = angular.fromJson(data.data);
+      console.dir(gradesData);
       var myChart = new dimple.chart(svg, gradesData);
 
       var x = myChart.addCategoryAxis("x", "lesson_title");
@@ -41,18 +42,66 @@ angular.module('classroom.grades', [])
       myChart.addSeries(null, dimple.plot.bubble);
       myChart.draw();
     });
+    
   } else { // The user is a student, so only show that student's grades
-    Grades.getForUser($rootScope.currentUser.username).then(function(data) {
-      var svg = dimple.newSvg(".grades", 1000, 800);
+    Grades.getAll().then(function(data) {
       var gradesData = angular.fromJson(data.data);
-      var myChart = new dimple.chart(svg, gradesData);
 
-      var x = myChart.addCategoryAxis("x", "lesson_title");
-      x.addOrderRule("lesson_title");
-      myChart.addMeasureAxis("y", "score");
-      myChart.addColorAxis("score",["#FF0000","#0000FF"]);
-      myChart.addSeries(null, dimple.plot.bar);
+      // Data to be edited for chart creation
+      var numAtScore = [
+        {rank: 10, num: 0}, {rank: 20, num: 0}, {rank: 30, num: 0}, {rank: 40, num: 0}, {rank: 50, num: 0},
+        {rank: 60, num: 0}, {rank: 70, num: 0}, {rank: 80, num: 0}, {rank: 90, num: 0}, {rank: 100, num: 0}
+      ];
+
+      // Modify info from gradesData and input into numAtScore for chart creation
+      gradesData.forEach(function(obj){
+        var range = 10;
+        while (obj.score > range){
+          range += 10;
+        }
+        numAtScore[ range.toString().slice(0,1)-1 ].num += 1;
+      });
+
+      // Create chart
+      var svg = dimple.newSvg(".grades", 700, 500);
+      var myChart = new dimple.chart(svg, numAtScore);
+      var x = myChart.addCategoryAxis("x", "rank");
+      myChart.addMeasureAxis("y", "num");
+      var s = myChart.addSeries(null, dimple.plot.line);
+      s.interpolation = "cardinal";
+      myChart.addLegend(60, 10, 500, 20, "right");
       myChart.draw();
     });
+
   }
+
+  // if ($scope.isTeacher()) {
+  //   Grades.getAll().then(function(data) {
+  //     var svg = dimple.newSvg(".grades", 1000, 800);
+  //     var gradesData = angular.fromJson(data.data);
+  //     console.dir(gradesData);
+  //     var myChart = new dimple.chart(svg, gradesData);
+
+  //     var x = myChart.addCategoryAxis("x", "lesson_title");
+  //     x.addOrderRule("lesson_title");
+  //     myChart.addCategoryAxis("y", "student");
+  //     myChart.addMeasureAxis("z", "score");
+  //     myChart.addColorAxis("score",["#FF0000","#0000FF"]);
+  //     myChart.addSeries(null, dimple.plot.bubble);
+  //     myChart.draw();
+  //   });
+  // } else { // The user is a student, so only show that student's grades
+  //   Grades.getForUser($rootScope.currentUser.username).then(function(data) {
+  //     var svg = dimple.newSvg(".grades", 1000, 800);
+  //     var gradesData = angular.fromJson(data.data);
+  //     var myChart = new dimple.chart(svg, gradesData);
+
+  //     var x = myChart.addCategoryAxis("x", "lesson_title");
+  //     x.addOrderRule("lesson_title");
+  //     myChart.addMeasureAxis("y", "score");
+  //     myChart.addColorAxis("score",["#FF0000","#0000FF"]);
+  //     myChart.addSeries(null, dimple.plot.bar);
+  //     myChart.draw();
+  //   });
+  // }
 }]);
