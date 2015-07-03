109cc32ffaf45af44ebec6737f56a97fd53f99a6
bdstein33 <bdstein33@gmail.com>
2015-07-02 21:14:44 -0700

    Add

--- server/routes/classes.server.routes.js
+++ server/routes/classes.server.routes.js
@@ -3,4 +3,6 @@ var classes = require('../controllers/classes.server.controller.js');
 module.exports = function(app) {
   app.get('/classes', classes.getClasses);
 
+  // app.post('/classes', classes.addClass);
+
 };
