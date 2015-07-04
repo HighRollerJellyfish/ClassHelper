7f389aa209fb9dfb9605939522a553bf5ac2e7fc
bdstein33 <bdstein33@gmail.com>
2015-07-03 17:13:13 -0700

    (feature) Add grade tables

--- client/app/gradebook/gradebook.js
+++ client/app/gradebook/gradebook.js
@@ -11,34 +11,21 @@ angular.module('classroom.gradebook', [])
 
   });
 
- 
-
-  // Grades.getClassGrades(1)
-  // .then(function(data) {
-  //   $scope.classGradesData = data.data
-
-  //   for(var i = 0; i < $scope.classGradesData.length; i++) {
-  //     $scope.assignments[$scope.classGradesData[i].assignment_title] = $scope.assignments[$scope.classGradesData[i].assignment_title] || {id: $scope.classGradesData[i].assignment_id, grades: []}
-  //     $scope.assignments[$scope.classGradesData[i].assignment_title].grades.push({
-  //       student_name: $scope.classGradesData[i].student_name,
-  //       grade: $scope.classGradesData[i].grade,
-  //       grade_id: $scope.classGradesData[i].grade_id
-  //     })
-  //   }
-  //   console.log($scope.assignments);
-  // })
-
 
   $scope.setActiveClass = function(class_id) {
     console.log("Active class: ", class_id);
     $scope.assignments = {};
+    $scope.activeAssignemnt = false;
+    $scope.activeAssignmentGrades = [];
+
     Grades.getClassGrades(class_id)
     .then(function(data) {
       $scope.classGradesData = data.data
 
       for(var i = 0; i < $scope.classGradesData.length; i++) {
-        $scope.assignments[$scope.classGradesData[i].assignment_title] = $scope.assignments[$scope.classGradesData[i].assignment_title] || {id: $scope.classGradesData[i].assignment_id, grades: []}
-        $scope.assignments[$scope.classGradesData[i].assignment_title].grades.push({
+        $scope.assignments[$scope.classGradesData[i].assignment_id] = $scope.assignments[$scope.classGradesData[i].assignment_id] || {title: $scope.classGradesData[i].assignment_title, grades: []}
+
+        $scope.assignments[$scope.classGradesData[i].assignment_id].grades.push({
           student_name: $scope.classGradesData[i].student_name,
           grade: $scope.classGradesData[i].grade,
           grade_id: $scope.classGradesData[i].grade_id
@@ -47,7 +34,11 @@ angular.module('classroom.gradebook', [])
     })
   }
 
-  $scope.setActiveAssignment = function(assignment_id)
+  $scope.setActiveAssignment = function(assignment_id) {
+    $scope.activeAssignment = $scope.assignments[assignment_id].title;
+    $scope.activeAssignmentGrades = [];
+    $scope.activeAssignmentGrades = $scope.assignments[assignment_id].grades;
+  }
 
 
 });
